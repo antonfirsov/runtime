57b43c4bef79f0820e227be32197a5228542b7fe
Anton Firszov <Anton.Firszov@microsoft.com>
2020-01-15 23:17:02 +0100

    better exception message

--- src/libraries/System.Net.Sockets/src/System/Net/Sockets/SafeSocketHandle.Windows.cs
+++ src/libraries/System.Net.Sockets/src/System/Net/Sockets/SafeSocketHandle.Windows.cs
@@ -71,7 +71,7 @@ namespace System.Net.Sockets
 
                         if (alreadyBound)
                         {
-                            throw new InvalidOperationException("Async operations are not allowed on a socket that's handle has been previously bound to a Thread Pool / IOCP port.");
+                            throw new InvalidOperationException("Asynchronous operations are not allowed on this socket. It's handle might have been previously bound to a Thread Pool / IOCP port.");
                         }
 
                         throw;
