62db5f0bdf6b81f124502e5cc3199876d77f101f
Anton Firszov <antonfir@gmail.com>
2020-05-13 18:35:45 +0200

    add ConsoleTest.csproj

---
+++ src/libraries/System.Net.Sockets/tests/ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
