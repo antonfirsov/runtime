de671d5edca2c6b80a608811b2afd0b837b2db28
Presentation account <presenter@corefx-mini.guest.corp.microsoft.com>
2020-07-29 13:21:55 -0700

    _Hello.cs

---
+++ src/libraries/System.Net.Sockets/tests/FunctionalTests/_Hello.cs
@@ -0,0 +1,25 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace System.Net.Sockets.Tests
+{
+    public class _Hello 
+    {
+        [Fact]
+        public void SuchSuccess()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void SuchFailure()
+        {
+            Assert.True(false);
+        }
+    }
+}
