ad0911933a84be467306b31e8781e0a35fce2cad
Anton Firszov <antonfir@gmail.com>
2020-11-10 17:20:08 +0100

    working repro

--- src/libraries/System.Net.Sockets/tests/FunctionalTests/_Repro.cs
+++ src/libraries/System.Net.Sockets/tests/FunctionalTests/_Repro.cs
@@ -11,31 +11,69 @@ using Xunit.Abstractions;
 
 namespace System.Net.Sockets.Tests
 {
-    public class _Repro
+    public class _Repro1 : _Repro { public _Repro1(ITestOutputHelper output) : base(output) { } }
+    public class _Repro2 : _Repro { public _Repro2(ITestOutputHelper output) : base(output) { } }
+    public class _Repro3 : _Repro { public _Repro3(ITestOutputHelper output) : base(output) { } }
+    public class _Repro4 : _Repro { public _Repro4(ITestOutputHelper output) : base(output) { } }
+    public class _Repro5 : _Repro { public _Repro5(ITestOutputHelper output) : base(output) { } }
+    public class _Repro6 : _Repro { public _Repro6(ITestOutputHelper output) : base(output) { } }
+    public class _Repro7 : _Repro { public _Repro7(ITestOutputHelper output) : base(output) { } }
+    public class _Repro8 : _Repro { public _Repro8(ITestOutputHelper output) : base(output) { } }
+
+    public abstract class _Repro
     {
         private readonly ITestOutputHelper _output;
-        private static SocketHelperArraySync _helper = new SocketHelperArraySync();
 
         public _Repro(ITestOutputHelper output)
         {
             _output = output;
         }
 
-        [Fact]
-        public void BurnCpu()
+        [Theory]
+        [InlineData(0)]
+        // [InlineData(1)]
+        // [InlineData(2)]
+        // [InlineData(3)]
+        // [InlineData(4)]
+        // [InlineData(5)]
+        // [InlineData(6)]
+        // [InlineData(7)]
+        // [InlineData(8)]
+        // [InlineData(9)]
+        // [InlineData(10)]
+        public void BurnCpu(int dummy)
         {
-            DoBurnCpu(60, 4);
+            DoBurnCpu(30 + dummy, 4);
         }
 
-        [Fact]
-        public async Task KillUdp()
+        public const int TestInstanceCount = 30;
+
+        public static TheoryData<bool, int> GetTestData()
+        {
+            TheoryData<bool, int> result = new TheoryData<bool, int>();
+            for (int i = 0; i < TestInstanceCount; i++)
+            {
+                result.Add(true, i);
+                result.Add(false, i);
+            }
+            return result;
+        }
+
+        private static SocketHelperBase CreateHelper(bool sync) => sync ?
+            new SocketHelperArraySync() :
+            new SocketHelperMemoryArrayTask();
+
+        [Theory(Timeout = 20000)]
+        [MemberData(nameof(GetTestData))]
+        public async Task KillUdp(bool sync, int dummy)
         {
             IPAddress leftAddress = IPAddress.Loopback, rightAddress = IPAddress.Loopback;
 
-            const int DatagramSize = 256;
+            int DatagramSize = 256 + dummy / 10000;
             const int DatagramsToSend = 256;
             const int AckTimeout = 2000;
             const int TestTimeout = 5000;
+            var helper = CreateHelper(sync);
 
             using var origLeft = new Socket(leftAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
             using var origRight = new Socket(rightAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
@@ -60,7 +98,7 @@ namespace System.Net.Sockets.Tests
                 var recvBuffer = new byte[DatagramSize];
                 for (int i = 0; i < DatagramsToSend; i++)
                 {
-                    SocketReceiveFromResult result = await _helper.ReceiveFromAsync(
+                    SocketReceiveFromResult result = await helper.ReceiveFromAsync(
                         left, new ArraySegment<byte>(recvBuffer), remote);
                     Assert.Equal(DatagramSize, result.ReceivedBytes);
                     Assert.Equal(rightEndpoint, result.RemoteEndPoint);
@@ -70,7 +108,9 @@ namespace System.Net.Sockets.Tests
                     receivedChecksums[datagramId] = Fletcher32.Checksum(recvBuffer, 0, result.ReceivedBytes);
 
                     receiverAck.Release();
+                    _output.WriteLine($"senderAck.WaitAsync [{i}] ...");
                     bool gotAck = await senderAck.WaitAsync(TestTimeout);
+                    _output.WriteLine($"senderAck.WaitAsync [{i}] returned");
                     Assert.True(gotAck, $"{DateTime.Now}: Timeout waiting {TestTimeout} for senderAck in iteration {i}");
                 }
             });
@@ -85,9 +125,11 @@ namespace System.Net.Sockets.Tests
                     random.NextBytes(sendBuffer);
                     sendBuffer[0] = (byte)i;
 
-                    int sent = await _helper.SendToAsync(right, new ArraySegment<byte>(sendBuffer), leftEndpoint);
+                    int sent = await helper.SendToAsync(right, new ArraySegment<byte>(sendBuffer), leftEndpoint);
 
+                    _output.WriteLine($"receiverAck.WaitAsync [{i}] ...");
                     bool gotAck = await receiverAck.WaitAsync(AckTimeout);
+                    _output.WriteLine($"receiverAck.WaitAsync [{i}] returned");
 
                     if (!gotAck)
                     {
@@ -128,7 +170,7 @@ namespace System.Net.Sockets.Tests
 
             while (DateTime.Now < end)
             {
-                Parallel.For(0, parallelism, i => _ = /*FindPrimeNumber(500 + i)*/ GetPi(1000, 100));
+                Parallel.For(0, parallelism, i => _ = FindPrimeNumber(500 + i));
             }
         }
 
