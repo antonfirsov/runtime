7ad7802da2ab520dbc2d71aa6c8e5b554b7e177c
antonfirsov <antonfir@gmail.com>
2025-02-15 19:20:57 +0100

    delete GetCookies_AddCookieVersion1WithExplicitDomain_CookieReturnedForDomainAndOneLevelSubDomain

--- src/libraries/System.Net.Primitives/tests/UnitTests/CookieContainerTest.cs
+++ src/libraries/System.Net.Primitives/tests/UnitTests/CookieContainerTest.cs
@@ -468,39 +468,6 @@ namespace System.Net.Primitives.Unit.Tests
         }
 
         [Fact]
-        public void GetCookies_AddCookieVersion1WithExplicitDomain_CookieReturnedForDomainAndOneLevelSubDomain()
-        {
-            const string SchemePrefix = "http://";
-            const string OriginalDomain = "contoso.com";
-            const string OriginalDomainWithLeadingDot = "." + OriginalDomain;
-
-            var container = new CookieContainer();
-            var cookie1 = new Cookie(CookieName1, CookieValue1) { Domain = OriginalDomainWithLeadingDot, Version = 1 };
-            container.Add(new Uri(SchemePrefix + OriginalDomain), cookie1);
-
-            var uri = new Uri(SchemePrefix + OriginalDomain);
-            CookieCollection cookies = container.GetCookies(uri);
-            Assert.Equal(1, cookies.Count);
-            Assert.Equal(OriginalDomainWithLeadingDot, cookies[CookieName1].Domain);
-
-            uri = new Uri(SchemePrefix + "www." + OriginalDomain);
-            cookies = container.GetCookies(uri);
-            Assert.Equal(1, cookies.Count);
-
-            uri = new Uri(SchemePrefix + "x.www." + OriginalDomain);
-            cookies = container.GetCookies(uri);
-            Assert.Equal(0, cookies.Count);
-
-            uri = new Uri(SchemePrefix + "y.x.www." + OriginalDomain);
-            cookies = container.GetCookies(uri);
-            Assert.Equal(0, cookies.Count);
-
-            uri = new Uri(SchemePrefix + "z.y.x.www." + OriginalDomain);
-            cookies = container.GetCookies(uri);
-            Assert.Equal(0, cookies.Count);
-        }
-
-        [Fact]
         public void Ctor_Capacity_Success()
         {
             CookieContainer cc = new CookieContainer(5);
