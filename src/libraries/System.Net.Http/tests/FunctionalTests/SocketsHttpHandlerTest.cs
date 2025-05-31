9d818631cb34e2d6a262e0f090f2ac70c20eae47
antonfirsov <antonfir@gmail.com>
2025-05-31 18:02:43 +0200

    GetAsync_TrailersWithoutServerStreamClosure_Success

--- src/libraries/System.Net.Http/tests/FunctionalTests/SocketsHttpHandlerTest.cs
+++ src/libraries/System.Net.Http/tests/FunctionalTests/SocketsHttpHandlerTest.cs
@@ -13,6 +13,7 @@ using System.Net.Sockets;
 using System.Net.Test.Common;
 using System.Numerics;
 using System.Reflection;
+using System.Reflection.PortableExecutable;
 using System.Runtime.CompilerServices;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
@@ -1264,6 +1265,42 @@ namespace System.Net.Http.Functional.Tests
 
             await stream.SendResponseHeadersAsync(statusCode: null, headers: trailers);
             stream.Stream.CompleteWrites();
+        }
+
+        // This is a regression test for https://github.com/dotnet/runtime/issues/60118.
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public async Task GetAsync_TrailersWithoutServerStreamClosure_Success(bool sendGarbageAfterTrailers)
+        {
+            SemaphoreSlim responseReceived = new SemaphoreSlim(0);
+            await LoopbackServerFactory.CreateClientAndServerAsync(async uri =>
+            {
+                using HttpClient client = CreateHttpClient();
+                HttpResponseMessage response = await client.GetAsync(uri);
+                Assert.Equal(TrailingHeaders.Count, response.TrailingHeaders.Count());
+                responseReceived.Release();
+                response.Dispose();
+            },
+            async server =>
+            {
+                await using Http3LoopbackConnection connection = (Http3LoopbackConnection)await server.EstablishGenericConnectionAsync();
+                await using Http3LoopbackStream stream = await connection.AcceptRequestStreamAsync();
+                _ = await stream.ReadRequestDataAsync();
+                await stream.SendResponseHeadersAsync(statusCode: HttpStatusCode.OK);
+                await stream.SendResponseBodyAsync(new byte[4096], isFinal: false);
+                await stream.SendResponseHeadersAsync(statusCode: null, headers: TrailingHeaders);
+                if (sendGarbageAfterTrailers)
+                {
+                    // https://datatracker.ietf.org/doc/html/rfc9114#section-7.2.8
+                    // Frame types of the format 0x1f * N + 0x21 for non - negative integer values of N are reserved to exercise the requirement that unknown types be ignored.
+                    await stream.SendFrameAsync(0x1f * 7 + 0x21, new byte[1024]);
+                }
+
+                await responseReceived.WaitAsync();
+                await stream.Stream.WritesClosed;
+                await stream.Stream.ReadsClosed;
+            });
         }
     }
 
